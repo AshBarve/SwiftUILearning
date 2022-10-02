2d9243c6a18f102f3a94c16c55768e49d2b69da4
Ashish Barve <ashishbarve44@gmail.com>
2022-10-02 16:19:17 +0400

    Lesson 7: Animated Onboarding Screen Components
    Added animation to the character image and the background rings on the onboarding screen

--- SwiftUI-SecondApp/SwiftUI-SecondApp/Screens/OnboardingView.swift
+++ SwiftUI-SecondApp/SwiftUI-SecondApp/Screens/OnboardingView.swift
@@ -14,6 +14,9 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var titleText: String = "Share."
+    @State private var indicatorOpacity: Double = 0.6
     
     var body: some View {
         ZStack {
@@ -25,10 +28,12 @@ struct OnboardingView: View {
                 Spacer()
                 VStack(spacing: 0) {
                 
-                    Text("Share.")
+                    Text(titleText)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
+                        .transition(.opacity)
+                        .id(titleText)
                     
                     Text("""
                         It's not how much we give but
@@ -49,13 +54,50 @@ struct OnboardingView: View {
                 
                 ZStack {
                     CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .offset(x: imageOffset.width * -1)
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                            titleText = "Give."
+                                        }
+                                    }
+                                })
+                                .onEnded({ gesture in
+                                    imageOffset = .zero
+                                    
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 0.6
+                                        titleText = "Share."
+                                    }
+                                })
+                        ) // GESTURE
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                 } // CENTER
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .regular, design: .rounded))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? indicatorOpacity : 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                    , alignment: .bottom
+                )
                 
                 Spacer()
                 
