e2b4b132b102e6ffa875ed6d3f07d8d4b1e42888
Ashish Barve <ashishbarve44@gmail.com>
2022-10-04 23:09:54 +0400

    Lesson 10: Adding Drag Gesture to Image

--- SwiftUI-ThirdApp/SwiftUI-ThirdApp/Screen/ContentView.swift
+++ SwiftUI-ThirdApp/SwiftUI-ThirdApp/Screen/ContentView.swift
@@ -11,6 +11,14 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    func resetImage() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     var body: some View {
         NavigationStack {
@@ -22,21 +30,39 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius:12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0).animation(.linear(duration: 1), value: isAnimating)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                     .onTapGesture(count: 2) {
-                        withAnimation(.spring()) {
-                            if imageScale == 1 {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
                                 imageScale = 5
-                            } else {
-                                imageScale = 1
                             }
+                        } else {
+                            resetImage()
                         }
                     }
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ gesture in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = gesture.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    withAnimation(.spring()) {
+                                        resetImage()
+                                    }
+                                }
+                            })
+                    )
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear {
-                isAnimating = true
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
             }
         }
     }
